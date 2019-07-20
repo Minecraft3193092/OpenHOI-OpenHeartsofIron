d27033f0f2a211b17d114762e81eb2343e5f702f
Thomas Reiser <reiser.thomas@gmail.com>
2019-07-20 11:22:42 +0200

    Fix Boost detection for Win64

--- cmake/global/GlobalDeps.cmake
+++ cmake/global/GlobalDeps.cmake
@@ -1,10 +1,13 @@
 find_package(Threads REQUIRED)
 
 
-set(Boost_USE_STATIC_LIBS OFF
+set(Boost_USE_STATIC_LIBS OFF)
 set(Boost_USE_STATIC_RUNTIME OFF)
+set(Boost_USE_DEBUG_LIBS OFF)
+set(Boost_USE_DEBUG_RUNTIME OFF)
 set(Boost_USE_MULTITHREADED ON)
 if(WIN32)
+    set(Boost_ARCHITECTURE "-x64")
     set(BOOST_ROOT ${CMAKE_SOURCE_DIR}/thirdparty/manual-build/precompiled/boost)
 endif()
 find_package(Boost 1.65 REQUIRED COMPONENTS program_options log locale)
