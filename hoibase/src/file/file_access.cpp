d9160b6366b9217bcb8754f422f7c16b17251197
Thomas Reiser <reiser.thomas@gmail.com>
2019-08-10 17:33:06 +0200

    Update file_access.cpp

--- hoibase/src/file/file_access.cpp
+++ hoibase/src/file/file_access.cpp
@@ -179,7 +179,7 @@ filesystem::path FileAccess::getOgrePluginDirectory() {
 
 #if defined(OPENHOI_OS_LINUX) || defined(OPENHOI_OS_BSD)
     Dl_info dlInfo;
-    dladdr(boost::format("libOgreMain.so.%d.%d.%d") % OGRE_VERSION_MAJOR % OGRE_VERSION_MINOR % OGRE_VERSION_PATCH),
+    dladdr(boost::format("libOgreMain.so.%d.%d.%d") % OGRE_VERSION_MAJOR % OGRE_VERSION_MINOR % OGRE_VERSION_PATCH,
            &dlInfo);
 
     filesystem::path libDir = filesystem::path(dlInfo.dli_fname).parent_path();
