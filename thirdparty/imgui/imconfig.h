b478f338895925745da8f07783512aff224c52d3
Thomas Reiser <reiser.thomas@gmail.com>
2019-12-07 15:55:13 +0100

    OGRE ImGui support on Windows

--- thirdparty/imgui/imconfig.h
+++ thirdparty/imgui/imconfig.h
@@ -1,6 +1,4 @@
 #pragma once
-
-#define IMGUI_DISABLE_OBSOLETE_FUNCTIONS
-
-#include <OgreOverlayPrerequisites.h>
-#define IMGUI_API _OgreOverlayExport
+    #define IMGUI_DISABLE_OBSOLETE_FUNCTIONS
+    #include <OgreOverlayPrerequisites.h>
+    #define IMGUI_API _OgreOverlayExport
