4d74a95c6508e7127117d243177263b1246c9d13
thomasreiser <reiser.thomas@gmail.com>
2019-12-08 17:05:00 +0100

    First unit test

--- hoibase/test/map/province.cpp
+++ hoibase/test/map/province.cpp
@@ -5,14 +5,13 @@
 namespace openhoi {
 
 TEST(Hoibase, MapProvinceCtor) {
-  /*auto coords = std::vector<std::vector<Ogre::Vector2>>();
+  auto coords = std::vector<std::vector<Ogre::Vector2>>();
   auto center = Ogre::Vector2(0.44f, 0.61f);
-  auto province =
-      new Province("some_province", coords, center);*/
+  auto province = new Province("some_province", coords, center);
 
-  //EXPECT_EQ(province->getID(), "some_province");
-  //EXPECT_EQ(province->getCenter(), center);
-  //EXPECT_EQ(province->getCoordinates(), coords);
+  EXPECT_EQ(province->getID(), "some_province");
+  EXPECT_EQ(province->getCenter(), center);
+  EXPECT_EQ(province->getCoordinates(), coords);
 }
 
 }  // namespace openhoi
