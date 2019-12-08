62173c4e840f5c5369070a5d2521b6caa492dde0
Thomas Reiser <reiser.thomas@gmail.com>
2019-12-08 16:29:12 +0100

    Unit tests: First steps

---
+++ cmake/global/GoogleTestDeps.cmake
@@ -0,0 +1,28 @@
+# Include FetchContent module
+include(FetchContent)
+
+
+# We want GTest 1.10.0
+set(GTEST_VERSION 1.10.0 CACHE STRING "Google test version")
+
+# Force GTest to build MD and not MT
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+
+# see https://cliutils.gitlab.io/modern-cmake/chapters/projects/fetch.html
+if(${CMAKE_VERSION} VERSION_LESS 3.14)
+    macro(FetchContent_MakeAvailable NAME)
+        FetchContent_GetProperties(${NAME})
+        if(NOT ${NAME}_POPULATED)
+            FetchContent_Populate(${NAME})
+            add_subdirectory(${${NAME}_SOURCE_DIR} ${${NAME}_BINARY_DIR})
+        endif()
+    endmacro()
+endif()
+
+
+# Fetch GTest
+FetchContent_Declare(googletest
+                     GIT_REPOSITORY https://github.com/google/googletest.git
+                     GIT_TAG release-${GTEST_VERSION})
+        
+FetchContent_MakeAvailable(googletest)
---
+++ hoibase/test/map/province.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+#include <hoibase/map/province.hpp>
+
+namespace openhoi {
+
+TEST(Hoibase, MapProvinceCtor) {
+  /*auto coords = std::vector<std::vector<Ogre::Vector2>>();
+  auto center = Ogre::Vector2(0.44f, 0.61f);
+  auto province =
+      new Province("some_province", coords, center);*/
+
+  //EXPECT_EQ(province->getID(), "some_province");
+  //EXPECT_EQ(province->getCenter(), center);
+  //EXPECT_EQ(province->getCoordinates(), coords);
+}
+
+}  // namespace openhoi
