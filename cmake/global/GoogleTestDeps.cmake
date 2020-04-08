966a9a60049f2b6f9dea8df8131808e525dff977
Thomas Reiser <reiser.thomas@gmail.com>
2020-04-08 17:19:43 +0200

    Fix CMake 3.10 support

--- cmake/global/GoogleTestDeps.cmake
+++ cmake/global/GoogleTestDeps.cmake
@@ -4,24 +4,43 @@ set(GTEST_VERSION 1.10.0 CACHE STRING "Google test version")
 # Force GTest to build MD and not MT
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 
-# see https://cliutils.gitlab.io/modern-cmake/chapters/projects/fetch.html
-if(${CMAKE_VERSION} VERSION_LESS 3.14)
-  macro(FetchContent_MakeAvailable NAME)
-      FetchContent_GetProperties(${NAME})
-      if(NOT ${NAME}_POPULATED)
-          FetchContent_Populate(${NAME})
-          add_subdirectory(${${NAME}_SOURCE_DIR} ${${NAME}_BINARY_DIR})
-      endif()
-  endmacro()
-else()
-  # Include FetchContent module
-  include(FetchContent)
-endif()
-
+set(GOOGLE_TEST_GIT_REPO https://github.com/google/googletest.git)
+set(GOOGLE_TEST_GIT_TAG release-${GTEST_VERSION})
 
 # Fetch GTest
-FetchContent_Declare(googletest
-                     GIT_REPOSITORY https://github.com/google/googletest.git
-                     GIT_TAG release-${GTEST_VERSION})
-        
-FetchContent_MakeAvailable(googletest)
+if(${CMAKE_VERSION} VERSION_GREATER_EQUAL 30.11)
+  # Include FetchContent module
+  include(FetchContent)
+
+  # see https://cliutils.gitlab.io/modern-cmake/chapters/projects/fetch.html
+  if(${CMAKE_VERSION} VERSION_LESS 3.14)
+    macro(FetchContent_MakeAvailable NAME)
+        FetchContent_GetProperties(${NAME})
+        if(NOT ${NAME}_POPULATED)
+            FetchContent_Populate(${NAME})
+            add_subdirectory(${${NAME}_SOURCE_DIR} ${${NAME}_BINARY_DIR})
+        endif()
+    endmacro()
+  endif()
+  
+  FetchContent_Declare(googletest
+                       GIT_REPOSITORY ${GOOGLE_TEST_GIT_REPO}
+                       GIT_TAG ${GOOGLE_TEST_GIT_TAG})
+          
+  FetchContent_MakeAvailable(googletest)
+else()
+  # Include ExternalProject module
+  include(ExternalProject)
+  
+  ExternalProject_Add(googletest
+    GIT_REPOSITORY ${GOOGLE_TEST_GIT_REPO}
+    GIT_TAG ${GOOGLE_TEST_GIT_TAG}
+    SOURCE_DIR "${CMAKE_CURRENT_BINARY_DIR}/googletest-src"
+    BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/googletest-build"
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+    TEST_COMMAND  ""
+  )
+endif()
+
