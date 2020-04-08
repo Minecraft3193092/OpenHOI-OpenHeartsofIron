b69212b76c463fd2d70f3b671a63cf901e8444f8
Thomas Reiser <reiser.thomas@gmail.com>
2020-04-08 15:49:50 +0200

    Fix Google Test CMake script

--- cmake/global/GoogleTestDeps.cmake
+++ cmake/global/GoogleTestDeps.cmake
@@ -1,7 +1,3 @@
-# Include FetchContent module
-include(FetchContent)
-
-
 # We want GTest 1.10.0
 set(GTEST_VERSION 1.10.0 CACHE STRING "Google test version")
 
@@ -10,13 +6,16 @@ set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 
 # see https://cliutils.gitlab.io/modern-cmake/chapters/projects/fetch.html
 if(${CMAKE_VERSION} VERSION_LESS 3.14)
-    macro(FetchContent_MakeAvailable NAME)
-        FetchContent_GetProperties(${NAME})
-        if(NOT ${NAME}_POPULATED)
-            FetchContent_Populate(${NAME})
-            add_subdirectory(${${NAME}_SOURCE_DIR} ${${NAME}_BINARY_DIR})
-        endif()
-    endmacro()
+  macro(FetchContent_MakeAvailable NAME)
+      FetchContent_GetProperties(${NAME})
+      if(NOT ${NAME}_POPULATED)
+          FetchContent_Populate(${NAME})
+          add_subdirectory(${${NAME}_SOURCE_DIR} ${${NAME}_BINARY_DIR})
+      endif()
+  endmacro()
+else()
+  # Include FetchContent module
+  include(FetchContent)
 endif()
 
 
